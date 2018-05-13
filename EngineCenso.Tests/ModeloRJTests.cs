a956a56e5d644ce7af1544bc3d05952c88792e45
Andre Barbosa Sampaio <andrebsamp@gmail.com>
2018-05-12 21:10:51 -0300

    Gera a resposta para RJ

---
+++ EngineCenso.Tests/ModeloRJTests.cs
@@ -0,0 +1,255 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EngineCenso.Tests
+{
+    [TestFixture]
+    public class ModeloRJTests
+    {
+        [TestCase]
+        public void InputNoModeloRJ_ComUmaCidadeEUmBairro_RetornaStringNoPadraoDefinido()
+        {
+            const string input = 
+@"<corpo>
+    <cidade>
+        <nome>Rio de Janeiro</nome>
+        <populacao>10345678</populacao>
+        <bairros>
+            <bairro>
+                <nome>Tijuca</nome>
+                <regiao>Zona Norte</regiao>
+                <populacao>135678</populacao>
+            </bairro>
+        </bairros>
+    </cidade>
+</corpo>";
+
+            const string expectedOutput =
+@"{
+""result"":[
+        {
+            ""cidade"":""Rio de Janeiro"",
+            ""habitantes"":10345678,
+            ""bairros"":[
+                {
+                    ""nome"":""Tijuca"",
+                    ""habitantes"":135678
+                }
+            ]
+        }
+    ]
+}";
+
+            EngineCenso engine = new EngineCenso();
+            var actualOutput = engine.Process(input);
+
+            // Don't care for spaces, tabs and new lines
+            string formatedExpectedOutput = Regex.Replace(expectedOutput, "\\s", "");
+            string formatedActualOutput = Regex.Replace(actualOutput, "\\s", "");
+
+            Assert.AreEqual(formatedExpectedOutput, formatedActualOutput);
+        }
+
+        [TestCase]
+        public void InputNoModeloRJ_ComUmaCidadeEDoisBairros_RetornaStringNoPadraoDefinido()
+        {
+            const string input =
+@"<corpo>
+    <cidade>
+        <nome>Rio de Janeiro</nome>
+        <populacao>10345678</populacao>
+        <bairros>
+            <bairro>
+                <nome>Tijuca</nome>
+                <regiao>Zona Norte</regiao>
+                <populacao>135678</populacao>
+            </bairro>
+            <bairro>
+                <nome>Botafogo</nome>
+                <regiao>Zona Sul</regiao>
+                <populacao>12456</populacao>
+            </bairro>
+        </bairros>
+    </cidade>
+</corpo>";
+
+            const string expectedOutput =
+@"{
+""result"":[
+        {
+            ""cidade"":""Rio de Janeiro"",
+            ""habitantes"":10345678,
+            ""bairros"":[
+                {
+                    ""nome"":""Tijuca"",
+                    ""habitantes"":135678
+                },
+                {
+                    ""nome"":""Botafogo"",
+                    ""habitantes"":12456
+                }
+            ]
+        }
+    ]
+}";
+
+            EngineCenso engine = new EngineCenso();
+            var actualOutput = engine.Process(input);
+
+            string formatedExpectedOutput = Regex.Replace(expectedOutput, "\\s", "");
+            string formatedActualOutput = Regex.Replace(actualOutput, "\\s", "");
+
+            Assert.AreEqual(formatedExpectedOutput, formatedActualOutput);
+        }
+
+        [TestCase]
+        public void InputNoModeloRJ_ComDuasCidadesEUmBairroCadaUma_RetornaStringNoPadraoDefinido()
+        {
+            const string input =
+@"<corpo>
+    <cidade>
+        <nome>Rio de Janeiro</nome>
+        <populacao>10345678</populacao>
+        <bairros>
+            <bairro>
+                <nome>Tijuca</nome>
+                <regiao>Zona Norte</regiao>
+                <populacao>135678</populacao>
+            </bairro>
+        </bairros>
+    </cidade>
+    <cidade>
+        <nome>Petrópolis</nome>
+        <populacao>300000</populacao>
+        <bairros>
+            <bairro>
+                <nome>Mosela</nome>
+                <regiao>Central</regiao>
+                <populacao>21234</populacao>
+            </bairro>
+        </bairros>
+    </cidade>
+</corpo>";
+
+            const string expectedOutput =
+@"{
+""result"":[
+        {
+            ""cidade"":""Rio de Janeiro"",
+            ""habitantes"":10345678,
+            ""bairros"":[
+                {
+                    ""nome"":""Tijuca"",
+                    ""habitantes"":135678
+                }
+            ]
+        },
+        {
+            ""cidade"":""Petrópolis"",
+            ""habitantes"":300000,
+            ""bairros"":[
+                {
+                    ""nome"":""Mosela"",
+                    ""habitantes"":21234
+                }
+            ]
+        }
+    ]
+}";
+
+            EngineCenso engine = new EngineCenso();
+            var actualOutput = engine.Process(input);
+
+            string formatedExpectedOutput = Regex.Replace(expectedOutput, "\\s", "");
+            string formatedActualOutput = Regex.Replace(actualOutput, "\\s", "");
+
+            Assert.AreEqual(formatedExpectedOutput, formatedActualOutput);
+        }
+
+        [TestCase]
+        public void InputNoModeloRJ_ComDuasCidadesEDoisBairrosCadaUma_RetornaStringNoPadraoDefinido()
+        {
+            const string input =
+@"<corpo>
+    <cidade>
+        <nome>Rio de Janeiro</nome>
+        <populacao>10345678</populacao>
+        <bairros>
+            <bairro>
+                <nome>Tijuca</nome>
+                <regiao>Zona Norte</regiao>
+                <populacao>135678</populacao>
+            </bairro>
+            <bairro>
+                <nome>Botafogo</nome>
+                <regiao>Zona Sul</regiao>
+                <populacao>12456</populacao>
+            </bairro>
+        </bairros>
+    </cidade>
+    <cidade>
+        <nome>Petrópolis</nome>
+        <populacao>300000</populacao>
+        <bairros>
+            <bairro>
+                <nome>Mosela</nome>
+                <regiao>Central</regiao>
+                <populacao>21234</populacao>
+            </bairro>
+            <bairro>
+                <nome>Retiro</nome>
+                <regiao>Central</regiao>
+                <populacao>51368</populacao>
+            </bairro>
+        </bairros>
+    </cidade>
+</corpo>";
+
+            const string expectedOutput =
+@"{
+""result"":[
+        {
+            ""cidade"":""Rio de Janeiro"",
+            ""habitantes"":10345678,
+            ""bairros"":[
+                {
+                    ""nome"":""Tijuca"",
+                    ""habitantes"":135678
+                },
+                {
+                    ""nome"":""Botafogo"",
+                    ""habitantes"":12456
+                }
+            ]
+        },
+        {
+            ""cidade"":""Petrópolis"",
+            ""habitantes"":300000,
+            ""bairros"":[
+                {
+                    ""nome"":""Mosela"",
+                    ""habitantes"":21234
+                },
+                {
+                    ""nome"":""Retiro"",
+                    ""habitantes"":51368
+                }
+            ]
+        }
+    ]
+}";
+
+            EngineCenso engine = new EngineCenso();
+            var actualOutput = engine.Process(input);
+
+            string formatedExpectedOutput = Regex.Replace(expectedOutput, "\\s", "");
+            string formatedActualOutput = Regex.Replace(actualOutput, "\\s", "");
+
+            Assert.AreEqual(formatedExpectedOutput, formatedActualOutput);
+        }
+
+    }
+}
---
+++ EngineCenso/CensoOutput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineCenso
+{
+    public class CensoOutput
+    {
+        public IEnumerable<Result> Result { get; set; }
+    }
+
+    public class Result
+    {
+        public string Cidade { get; set; }
+        public int Habitantes { get; set; }
+        public IEnumerable<Bairro> Bairros { get; set; }
+    }
+
+    public class Bairro
+    {
+        public string Nome { get; set; }
+        public int Habitantes { get; set; }
+    }
+}
