07301225386773ac8cbf2ff674852c847d2f9f3a
Andre Barbosa Sampaio <andrebsamp@gmail.com>
2018-05-21 01:09:16 -0300

    Using exception handler middleware

--- EngineCenso.RestApi/Startup.cs
+++ EngineCenso.RestApi/Startup.cs
@@ -62,10 +62,7 @@ namespace EngineCenso.RestApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseExceptionHandler();
             app.UseAuthentication();
             app.UseMvc();
         }
